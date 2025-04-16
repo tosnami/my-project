46525fe904926a7a5bf2da0eed8ebc786129dde6
tosnami <ahmedtosnami@gmail.com>
2025-04-16 03:46:27 +0200

    ddd

--- shop-clothes/vite.config.js
+++ shop-clothes/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/alx-shop-project/shop-clothes/', // علشان المسار يكون صح على GitHub Pages
+  base: '/alx-shop-project/shop-clothes/', // علشان المسار يكون صح على GitHub Pagesekdhbekdcedfkwdb
 })
