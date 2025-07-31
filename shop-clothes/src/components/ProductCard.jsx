d7e5c2c36324d0037837259e0e56c6fadca9ef0a
tosnami <ahmedtosnami@gmail.com>
2025-07-31 04:34:55 +0300

    ddd

--- shop-clothes/src/components/ProductCard.jsx
+++ shop-clothes/src/components/ProductCard.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const ProductCard = ({ product }) => {
   const handleAddToCart = () => {
     const cart = JSON.parse(localStorage.getItem('cart')) || [];
@@ -7,8 +9,8 @@ const ProductCard = ({ product }) => {
       name: product.title,
       image: product.image,
       price: product.price,
-      size: "Large",    
-      color: "White" 
+      size: "Large",
+      color: "White"
     };
 
     cart.push(newItem);
@@ -19,9 +21,12 @@ const ProductCard = ({ product }) => {
 
   return (
     <div className="border p-4 rounded">
-      <img src={product.image} alt={product.title} className="w-full h-48 object-contain" />
-      <h3 className="text-lg font-semibold mt-2">{product.title}</h3>
-      <p className="text-gray-600">${product.price}</p>
+      <Link to={`/product/${product.id}`}>
+        <img src={product.image} alt={product.title} className="w-full h-48 object-contain" />
+        <h3 className="text-lg font-semibold mt-2">{product.title}</h3>
+        <p className="text-gray-600">${product.price}</p>
+      </Link>
+
       <button
         onClick={handleAddToCart}
         className="bg-black text-white px-4 py-2 mt-2 rounded hover:bg-gray-800"
